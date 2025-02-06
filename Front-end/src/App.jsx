0f457a2697574d8e267ddfccd4c9fb9c1227e143
tungvt <tungvt@fds.vn>
2025-02-06 16:19:01 +0700

    first commit

---
+++ Front-end/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const handleClickCount = ()=>{
+    setCount((prevCount) => prevCount + 1)
+  }
+
+  return (
+    <>
+      <h1>Count state</h1>
+      <p>{count}</p>
+      <button onClick={handleClickCount}>Tăng</button>
+    </>
+  )
+}
+
+export default App
